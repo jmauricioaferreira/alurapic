03ac9c3be4395f194b4419eeb89ac422decf0a42
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-09-02 20:11:13 -0300

    Iniciando a aplicação alurapic

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  photos = [
+    {
+      url: 
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg/1024px-Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg",
+      description: 'Praia'
+    },
+    
+    {
+      url: 
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg/1024px-Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg",
+      description: 'Praia'
+    },
+    
+    {
+      url: 
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg/1024px-Korfu_%28GR%29%2C_Korfu%2C_Altstadt_--_2018_--_1156.jpg",
+      description: 'Praia'
+    },
+    
+
+  ];
+ 
+}
